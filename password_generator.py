0098698d5e1b5e5c24db26d5206a36e0cea8d586
NezihBoujdidi <136797460+NezihBoujdidi@users.noreply.github.com>
2025-07-28 19:13:38 +0100

    Create password_generator.py
    * learned re module, random and secrets
    * re to use re.findall(pattern, str) that will search all occurences of the pattern in the str
    * secrets is more secure than random, less obvious, secrets.choice(list) that will choose a random element from the list, there is also random.choice() but less secure
    * string.ascii_letters [a-zA-Z]
    * string.digits r'\d' (raw string)
    * string.punctuation contains most of '[^a-zA-Z0-9_]'

---
+++ password_generator.py
@@ -0,0 +1,45 @@
+** r'' : raw string, \ is not a backslash here, we can use r'\d' for digits and r'\w' for alphanumeric and \D and \W for the opposite, same as '[^0-9]' for exemple = '\D' **
+import re
+import secrets
+import string
+
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        #pattern = r'\W'
+        #quote = '_', it counts as '\w' (alphanumiric)
+        #print(re.findall(pattern, quote)) --> None
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
+    return password
+    
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
