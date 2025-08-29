3d64c66e910f2d1a8a99d752e0ff75758b4398ea
NezihBoujdidi <136797460+NezihBoujdidi@users.noreply.github.com>
2025-08-29 14:23:30 +0100

    Create probability_calculator.py

---
+++ probability_calculator/probability_calculator.py
@@ -0,0 +1,60 @@
+import copy
+import random
+
+class Hat:
+    __slots__ = ('__contents')
+
+    def __init__(self, **args):
+        if len(args) == 0:
+            raise TypeError(f"the {__class__.__name__} class takes at least one argument of a color but 0 were given")
+        self.__contents = []
+        for color , i in args.items():
+            for _ in range(i):
+                self.__contents.append(color)
+        print(self.__contents)
+
+
+    @property
+    def contents(self):
+        return self.__contents
+    
+    @contents.setter
+    def contents(self, value):
+        self.__contents = value
+
+
+    def draw(self, number):
+        removed_balls = []
+        for _ in range(number):
+            if len(self.__contents) == 0 :
+                break   
+            removed_ball = self.__contents.pop(random.randint(0,len(self.__contents)-1))
+            removed_balls.append(removed_ball)
+        return removed_balls
+            
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    if not isinstance(hat, Hat):
+        raise TypeError(f"{hat} should be an instance of {Hat.__name__}")
+    balls = copy.deepcopy(hat.contents)
+    m = 0
+    for _ in range(num_experiments):
+        match = 0
+        hat.contents = copy.deepcopy(balls)
+        removed_balls = hat.draw(num_balls_drawn)
+        for color , value in expected_balls.items():
+            if removed_balls.count(color) < value :
+                break
+            else:
+                match += 1
+        if match == len(expected_balls) :
+            m += 1
+    return m/num_experiments
+
+hat = Hat(black=6, red=4, green=3)
+probability = experiment(hat=hat,
+                  expected_balls={'red':2,'green':1},
+                  num_balls_drawn=5,
+                  num_experiments=20)
+print(probability)
