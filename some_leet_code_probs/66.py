561bbd3a604558c8199924b5195c02a6dd8d0d6b
NezihBoujdidi <136797460+NezihBoujdidi@users.noreply.github.com>
2026-04-03 22:57:07 +0100

    Add plusOne method to increment digits
    Implement plusOne method to increment a list of digits.

---
+++ some_leet_code_probs/66.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i= len(digits)-1
+        digits[i] = (digits[i]+1) % 10 
+        while (digits[i] == 0):
+            if (i!=0):
+                digits[i-1] = (digits[i-1]+1) % 10
+                i-=1
+            if (i==0 and digits[i] == 0):
+                digits.append(0)
+                for j in range(len(digits)-1,1,-1):
+                    digits[j] = digits[j-1]
+                digits[0] = 1
+        return digits
