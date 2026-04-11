ab344baa69b17b1d8b5f70ad0e6df4d2c0b5154b
NezihBoujdidi <136797460+NezihBoujdidi@users.noreply.github.com>
2026-04-11 22:53:22 +0100

    Optimize intersection method with set data structure
    Refactor intersection method to use set for faster lookups.

--- some_leet_code_probs/349.py
+++ some_leet_code_probs/349.py
@@ -5,3 +5,17 @@ class Solution:
             if num in nums2 and num not in intersection:
                 intersection.append(num)
         return intersection
+
+//Faster solution
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = set()
+        nums2 = set(nums2)
+        for num in nums1:
+            if num in nums2 and num not in intersection:
+                intersection.add(num)
+        intersectionArr = []
+        for num in intersection:
+            intersectionArr.append(num)
+        return intersectionArr
